6dc895dc95b1334d7612cd0c12c9567fede7bd7c
Tejasvi Kothapalli <tejasvi.kothapalli@gmail.com>
2016-07-02 14:54:05 -0700

    updated client

--- client/client_home.py
+++ client/client_home.py
@@ -14,9 +14,10 @@ import nmap
 
 
 nm = nmap.PortScanner()
-home_id = "homeid1"
+home_id = "teja"
 network_addr_to_scan = "192.168.1.1/24"
-url = "http://localhost:8000/smart_home_app/?"
+#url = "http://localhost:8000/smart_home_app/?"
+url = "http://54.201.150.12:8043/smart_home_app/?"
 
 # sample document structure
 #task = {"home_id": home_id,
@@ -33,7 +34,9 @@ import time
 # {"home_id": "homeid1", "device_visibility": {"192.168.1.118": {"38": "1"}, "192.168.1.102": {"38": "1"}, "192.168.1.104": {"38": "1"}, "192.168.1.106": {"38": "1"}, "192.168.1.146": {"38": "1"}, "192.168.1.1": {"38": "1"}}, "timestamp_hour": "2016-02-07T17:38:00"}
 
 # loop every minute.
-for count in range(1, 100000):
+#for count in range(1, 100000):
+while True:
+    #time.sleep(60)
     time.sleep(60)
     nm.scan(hosts=network_addr_to_scan, arguments='-sP')
     print nm.all_hosts()
@@ -47,9 +50,7 @@ for count in range(1, 100000):
     home_doc_map = {}
     device_visibility_doc_map = {}
     # device_visibility_doc_map:-
-    # { "d1": {"10": 1},
-    #  "d2": {"10":1 }
-    #  }
+    # "device_visibility": {"192_168_1_118": {"52": "1"}, "192_168_1_119": {"52": "1"}, "192_168_1_137": {"52": "1"}, "192_168_1_126": {"52": "1"}, "192_168_1_1": {"52": "1"}, "192_168_1_145": {"52": "1"}}
     for device in visible_hosts:
         minute_value_map[minute] = "1"
         # convert . to _ in the ip address
@@ -61,13 +62,41 @@ for count in range(1, 100000):
         home_doc_map["home_id"] = home_id
         home_doc_map["timestamp_hour"] = curr_time.isoformat()
         home_doc_map["device_visibility"] = device_visibility_doc_map
-        home_doc_json = json.dumps(home_doc_map)
-        print(home_doc_json)
-        if len(device_visibility_doc_map) > 0:
-            # test = 1
-            # we have some device visible, so post it to the server.
-            resp = requests.post(url, data=home_doc_json)
-            print(resp)
-            if resp.status_code != 201 and resp.status_code != 200:
-                raise ApiError('POST /tasks/ {}'.format(resp.status_code))
-                # print('Created task. ID: {}'.format(resp.json()["id"]))
+
+    # Add wemo power states also
+    import urllib2
+    import json
+    wemo_url_list  = ["http://localhost:5000/api/device/WeMo%20Insight"]
+
+    for wemo in wemo_url_list:
+        try :
+            minute_value_map[minute] = "1"
+            response = urllib2.urlopen(wemo).read()
+            #print "wemo response" + wemo + ":" + response
+            json_obj = json.loads(response)
+            # is the wemo powered on ?
+            #print "wemo current state" + str(json_obj["state"])
+            if json_obj["state"] == 1:
+                # get the last port of the wemo after "/" eg., WeMo%20Insight
+                formatted_device = wemo.rsplit('/', 1)[-1]
+                device_visibility_doc_map[formatted_device] = minute_value_map 
+                home_doc_map["device_visibility"]  =  device_visibility_doc_map 
+
+        except:
+            # ignore not able to access the wemo device
+            pass
+
+
+    # send the info to a server
+    home_doc_json = json.dumps(home_doc_map)
+    print(home_doc_json)
+    if len(device_visibility_doc_map) > 0:
+        # test = 1
+        # we have some device visible, so post it to the server.
+        resp = requests.post(url, data=home_doc_json, headers={'Connection':'close'})
+        #print(resp)
+        if resp.status_code != 201 and resp.status_code != 200:
+            print (resp.status_code)
+            pass
+            #raise ApiError('POST /tasks/ {}'.format(resp.status_code))
+            # print('Created task. ID: {}'.format(resp.json()["id"]))
