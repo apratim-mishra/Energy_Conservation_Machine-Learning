d4ada9c771072501f7da12f34284fcd6ab4f03a3
Ubuntu <ubuntu@ip-172-31-41-212.us-west-2.compute.internal>
2016-11-19 18:38:12 +0000

    added more machines learning analysis code

--- smart_home_app/views.py
+++ smart_home_app/views.py
@@ -5,6 +5,7 @@ from pymongo import MongoClient
 import json
 import datetime
 import pymongo
+import subprocess
 from django.http import JsonResponse
 
 #@csrf_exempt
@@ -15,9 +16,70 @@ def smart_home_api(request):
     # get a connection to our database
     dbconn = db.home_automation  # should be database name
     device_stats_collection = dbconn['device_stats']  # collection in DB
-    #print "HELLO"
 
-    if request.method == 'GET':
+
+    if request.method == 'GET' and request.GET['op'] == 'compute_clusters':
+        day=request.GET['day'] # eg., "2016-08-20"
+	home_id = "teja"
+        cmd = "/home/ubuntu/spark-1.5.2-bin-hadoop2.6/bin/pyspark --executor-memory 100M --jars /home/ubuntu/Energy_Conservation_Machine-Learning/spark-mongo-libs/mongo-hadoop-core-1.4.2.jar,/home/ubuntu/Energy_Conservation_Machine-Learning/spark-mongo-libs/mongo-java-driver-2.13.2.jar  /home/ubuntu//Energy_Conservation_Machine-Learning/services/cluster-home-devices-by-day.py " + day
+
+        p = subprocess.Popen(cmd, stdout=subprocess.PIPE, shell=True).communicate()
+        #p.wait(timeout=3000)
+        return Response(p)        
+
+    if request.method == 'GET' and request.GET['op'] == 'hourly_data':
+        # For every one should the visitbility data
+        # Eg., db.device_clusters.find({"timestamp_hour":"2016-07-01T22:00:00", "home_id":"teja"},{"device_visibility_by_minute":1,"_id":0 } ).pretty()
+        # db.device_clusters.find({"timestamp_hour":"2016-07-01T02:00:00", "home_id":"teja"},{"device_visibility_by_minute":1,"_id":0 } ).pretty()
+        day=request.GET['day'] # eg., "2016-08-20"
+	home_id = "teja"
+        db = MongoClient('localhost', 27017)
+        dbconn = db.home_automation  # should be database name
+        device_stats_collection = dbconn['device_stats']  # collection in DB
+        #hour = day + "T01:00:00"
+        hour = "2016-07-01T02:00:00"
+        hour = day + "T" + "02" + ":00:00"
+        out = ""
+        for count in range(0,24): # hour
+            hour = day + "T" + "%02d" % count  + ":00:00"
+            #print day + "T" + "%02d" % count  + ":00:00"
+
+            print hour
+            
+            device_cluster_map = {}
+            # find the device and its cluster for the hour
+            cursor = dbconn.device_clusters.find({"timestamp_hour": hour, "home_id" : home_id }, {"device_clusters":1,"_id":0 } )
+            for document in cursor:
+                for device in document['device_clusters']:  
+                    #print device + " " + str(document['device_clusters'][device])
+                    device_cluster_map[device] = document['device_clusters'][device]
+
+            # now find the device visitibility for the hour (for every minute)
+            cursor = dbconn.device_clusters.find({"timestamp_hour": hour, "home_id" : home_id }, {"device_visibility_by_minute":1,"_id":0 } )
+
+
+            out =  out + "<table border=0>"
+
+            out = out + "<tr>"
+            out = out + "<td>" +  hour + "</td>"
+            out = out + "<td>" +  "cluster" + "</td>"
+            for minute in range(1,60):
+                out = out + "<td>" +  str(minute) + "</td>"
+            out = out + "</tr>"
+
+            for document in cursor:
+                for device in document['device_visibility_by_minute']:
+                    out = out + "<tr>"
+                    out = out + "<td>" +  device + "</td>" 
+                    out = out + "<td>cluster" +  str(device_cluster_map[device]) + "</td>" 
+                    for minute_visibility in document['device_visibility_by_minute'][device]:
+                        out = out + "<td>" + str(minute_visibility) + "</td>"
+                out = out + "</tr>"
+            out =  out + "</table>"
+            #print out
+        return Response(out)
+
+    if request.method == 'GET' and request.GET['op'] == 'graph':
 
 # returns data in this format:-
 # {"cols":[{"type":"string","id":"IP_Address"},{"type":"date","id":"Start"},{"type":"date","id":"End"}],"rows":[
@@ -26,7 +88,13 @@ def smart_home_api(request):
 # {"c":[{"v":"192_168_1_107"},{"v":"Date(2016,06,13,00,39,0)"},{"v":"Date(2016,06,13,00,39,0)"}]},
 # ]}
 
-        day = "2016-07-01"
+        # from 2016-06-25 - 2016-07-02
+        # "2016-06-28", "2016-06-27", "2016-06-26", 06-29, 06-30, 07-01, 07-02
+         
+        #day = "2016-07-01"
+        #day = "2016-06-28"
+        #day = "2016-08-20"
+        day=request.GET['day'] # eg., "2016-08-20"
 	home_id = "teja"
         db = MongoClient('localhost', 27017)
         dbconn = db.home_automation  # should be database name
@@ -36,6 +104,7 @@ def smart_home_api(request):
 #cursor = dbconn.device_stats.find({"timestamp_hour" : "2016-06-10T22:00:00"})
         output = ""
         output += '{"cols":[{"type":"string","id":"IP_Address"},{"type":"string","id":"Cluster_Name"},{"type":"date","id":"Start"},{"type":"date","id":"End"}], \n "rows":['
+
         for document in cursor:
                 devicesList = document['device_visibility']
 
@@ -79,7 +148,10 @@ def smart_home_api(request):
         output = output[:last_occurance] + "" + output[last_occurance+1:]
         
         #print output
-        json_obj = json.loads(output)
+        try:
+            json_obj = json.loads(output)
+        except:
+            print output
         return JsonResponse(json_obj)
 
     if request.method == 'POST':
